3361c179616ce0534c012a7b929c258036ca998d
José Rosenbluth <Jos Rosenbluth>
2017-04-30 02:25:18 -0400

    fix: added instructions and audio for 2 remaining stations

--- NightAtTheMuseum/Assets/scripts/gameStart.cs
+++ NightAtTheMuseum/Assets/scripts/gameStart.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 
 public class gameStart : MonoBehaviour {
 
+	public GameObject canvas1;
+	public GameObject canvas2;
+
+	public void onClicked1()
+	{
+		canvas1.SetActive (false);
+		canvas2.SetActive (true);
+	}
+
 	public void onClicked()
 	{
-		SceneManager.LoadScene(1);
+		SceneManager.LoadScene (1);
 	}
 }
